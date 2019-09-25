c9765bc0caa17a932150687784a35e9830ddcdb0
JeetKhatri <jeet.g.khatri@gmail.com>
2019-09-25 15:15:32 +0530

    backend

---
+++ angular-springboot-backend/src/main/java/com/AngularSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootBackendApplication.class, args);
+	}
+
+}
